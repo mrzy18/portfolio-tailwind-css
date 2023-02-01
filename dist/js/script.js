1e2476c9163d7c34925c13cbb891de54e60081fa
mrzy18 <mrzy18@gmail.com>
2023-02-01 17:16:43 +0700

    Penambahan fitur dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,19 +1,56 @@
 // Navbar Fixed
 window.onscroll = () => {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
-    if (window.pageYOffset > fixedNav) {
-        header.classList.add('nav-fixed');
-    } else {
-        header.classList.remove('nav-fixed');
-    }
-}
+	const header = document.querySelector('header');
+	const fixedNav = header.offsetTop;
+	const toTop = document.querySelector('#to-top');
+	if (window.pageYOffset > fixedNav) {
+		header.classList.add('nav-fixed');
+		toTop.classList.remove('hidden');
+		toTop.classList.add('flex');
+	} else {
+		header.classList.remove('nav-fixed');
+		toTop.classList.remove('flex');
+		toTop.classList.add('hidden');
+	}
+};
 
 // Hamburger
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
+hamburger.addEventListener('click', function () {
+	hamburger.classList.toggle('hamburger-active');
+	navMenu.classList.toggle('hidden');
 });
+
+window.addEventListener('click', function (e) {
+	if (e.target != hamburger && e.target != navMenu) {
+		hamburger.classList.remove('hamburger-active');
+		navMenu.classList.add('hidden');
+	}
+});
+
+// Dark mode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+	if (darkToggle.checked) {
+		html.classList.add('dark');
+		localStorage.theme = 'dark';
+	} else {
+		html.classList.remove('dark');
+		localStorage.theme = 'light';
+	}
+});
+
+// pindahkan posisi toggle sesuai mode
+if (
+	localStorage.theme === 'dark' ||
+	(!('theme' in localStorage) &&
+		window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+	darkToggle.checked = true;
+} else {
+	darkToggle.checked = false;
+}
